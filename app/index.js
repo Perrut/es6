068a3d31838fe687159dc76f3b2df53283191fe1
Matheus Perrut <matheus.perrut@govbr.com.br>
2018-09-11 18:06:36 -0300

    Setando webpack hot module replacement

--- app/index.js
+++ app/index.js
@@ -1,9 +1,6 @@
-import ImageMessage from './models/image-message.model';
+import Message from './models/message.model';
 
-const m = new ImageMessage();
-
-console.log('not empty');
-console.dir(m);
-console.log('updated');
+console.log('Index started');
+console.dir(new Message());
 /* webpack-dev-server roda o projeto na memória, atualiza sozinho, mas só client-side, se envolver 
 servidor não rola, não é servidor web completo */
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 const config = {
   entry: './app/index.js',
@@ -10,8 +11,16 @@ const config = {
 }
 
 if (process.env.NODE_ENV === 'development') {
-  config.watch = true,
-  config.devtool = 'source-map'
+  config.watch = true;
+  config.devtool = 'source-map';
+} else if (process.env.NODE_ENV === 'hot') {
+  config.devtool = 'source-map';
+  config.devServer = {
+    hot: true,
+  };
+  config.plugins = [
+    new webpack.HotModuleReplacementPlugin(),
+  ]
 }
 
 module.exports = config;
